e6a08fb906af30b9961f0d603d93b8db6e7c39cb
El Hadji Alassane DIENG <86415543+diengElhadjiAlassane@users.noreply.github.com>
2021-10-27 02:51:47 +0000

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://aloupapa:alabaye@cluster0.3ed5o.mongodb.net/test?authSource=admin&replicaSet=atlas-1296wq-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true",
+  secretOrKey: "secret",
+};
